0e862dd1681405e7b08bc87a6d92fe825679c47a
Mattias Soderberg <mattiassoderberj@gmail.com>
2021-03-28 12:45:00 +0200

    changed app code and webpage

--- web-app/app.py
+++ web-app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 import random
 
-tanzu_quotes = ['Tanzu Rules', 'Tanzu World', 'Tanzu Rocks', 'Tanzu is Swahili for branches']
+tanzu_quotes = ['Tanzu Rules', 'Tanzu World', 'Tanzu Rocks', 
+'Tanzu is Swahili for branches', 'VMware Tanzu']
 
 app = Flask(__name__)
 
@@ -9,7 +10,7 @@ app = Flask(__name__)
 
 def index():
     show_text = random.choice(tanzu_quotes)
-    return render_template("index2.html", count = show_text) 
+    return render_template("index2.html", wordofday = show_text) 
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=80)
