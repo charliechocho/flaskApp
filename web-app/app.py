8b1b1199a8fd6c63b018d1c7843d61d68707939b
Mattias Soderberg <msoderberg@msoderberg-a02.vmware.com>
2021-04-05 17:42:59 +0200

    Changed code in app.py and index2.html

--- web-app/app.py
+++ web-app/app.py
@@ -3,6 +3,7 @@ import random
 
 tanzu_quotes = ['Tanzu Rules', 'Tanzu World', 'Tanzu Rocks', 
 'Tanzu is Swahili for branches', 'VMware Tanzu']
+tanzu_colors = ['red', 'blue', 'green', 'yellow', 'brown']
 
 app = Flask(__name__)
 
@@ -10,7 +11,8 @@ app = Flask(__name__)
 
 def index():
     show_text = random.choice(tanzu_quotes)
-    return render_template("index2.html", wordofday = show_text) 
+    color_text = random.choice(tanzu_colors)
+    return render_template("index2.html", wordofday = show_text, tcolor = color_text) 
 
 if __name__ == "__main__":
     app.run(debug = True, host="0.0.0.0", port=80)
